6eac73012e334826c0b4316e85102fc0809a0cac
258030 <258030@usn.no>
2025-04-27 00:54:46 +0200

    lagd repo

---
+++ CleanMainDataSet.R
@@ -0,0 +1,88 @@
+getwd()
+setwd("DAT3000/Eksamen")
+# Cleaning the original dataset
+{
+  library(dplyr)
+
+    df <- as.data.frame(read.csv("pcos_dataset.csv"))
+    
+    df$BMI_Over_Normal <- ifelse(df$BMI > 25, 1, 0)
+    df$Testosterone_Over_Normal <- ifelse(df$Testosterone_Level.ng.dL. > 70, 1, 0)
+    df$Follicle_Less_Than_Average <- ifelse(df$Antral_Follicle_Count < 20, 1, 0)
+    
+    df <- df %>%
+      relocate(BMI_Over_Normal, .after = BMI) %>%
+      relocate(Testosterone_Over_Normal, .after = Testosterone_Level.ng.dL.) %>%
+      relocate(Follicle_Less_Than_Average, .after = Antral_Follicle_Count)
+    
+    View(df)
+}  
+    write.csv(df, paste0("pcos_cleaned.csv"), row.names = FALSE)
+
+# cleaning and and creating our own subset for predictions using nhanes 2015/2016 (latest iteration that had testosterone in their exams)   
+{
+  library(haven)
+
+    bmi <- read_xpt("raw/BMX_I.xpt")
+    demography <- read_xpt("raw/DEMO_I.xpt")
+    hormones <- read_xpt("raw/TST_I.xpt")
+    
+    newData <- demography %>%
+      inner_join(hormones, by = "SEQN") %>%
+      inner_join(bmi, by = "SEQN")
+    
+    newData <- replace(newData, is.na(newData), 0)
+    newData <- subset(newData, RIAGENDR == 2)
+    newData <- subset(newData, RIDAGEYR <= 46)
+    newData <- subset(newData, RIDAGEYR >= 18)
+    
+    newData$BMI_Over_Normal <- ifelse(newData$BMXBMI > 25, 1, 0)
+    newData$Testosterone_Over_Normal <- ifelse(newData$LBXTST > 70, 1, 0)
+    
+    newData <- newData %>%
+      relocate(BMI_Over_Normal, .after = BMXBMI) %>%
+      relocate(Testosterone_Over_Normal, .after = LBXTST)
+}
+    
+    write.csv(newData, paste0("nhanes_without_selection.csv"), row.names = FALSE)
+    
+{
+    newData$Number_Of_Children_Under_18 <- rowSums(newData[, c("DMDHHSZA", "DMDHHSZB")])
+    newData <- subset(newData, select=c(RIDAGEYR, RIDRETH3, Number_Of_Children_Under_18, LBXTST, Testosterone_Over_Normal, BMXBMI, BMI_Over_Normal))
+    newData <- newData %>%
+      rename(
+        Age = RIDAGEYR,
+        Race = RIDRETH3,
+        Testosterone_Level.ng.dL. = LBXTST,
+        BMI = BMXBMI
+      )
+}
+    
+    write.csv(newData, paste0("nhanes_with_selection.csv"), row.names = FALSE)
+{
+  
+  mainDf <- as.data.frame(read.csv("pcos_cleaned.csv"))
+  secondDf <- as.data.frame(read.csv("nhanes_with_selection.csv"))
+  
+  mainDf <- mainDf %>%
+    select(-Menstrual_Irregularity,
+           -Antral_Follicle_Count,
+           -Follicle_Less_Than_Average)
+  secondDf <- secondDf %>%
+    select(-Race, -Number_Of_Children_Under_18)
+  
+  secondDf <- secondDf %>%
+    relocate(BMI, .after = Age) %>%
+    relocate(BMI_Over_Normal, .after = BMI)
+  
+  mainDf$PCOS_Diagnosis <- ifelse(mainDf$PCOS_Diagnosis == 1, "Yes", "No")
+  
+  
+  View(mainDf)
+  View(secondDf)
+    }
+    
+    write.csv(mainDf, paste0("mainDf.csv"), row.names = FALSE)
+    write.csv(secondDf, paste0("nhanes_ex.csv"), row.names = FALSE)
+  
+
